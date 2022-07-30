72a1fd593120faae622a179f5ad6137ebfcf0e9c
Vlad Kovalov <vlad.kovalov.92@gmail.com>
2022-07-30 20:10:50 +0300

    added flip card func

--- scripts/createGameCard.js
+++ scripts/createGameCard.js
@@ -1,17 +1,20 @@
 export function createGameCard(cardDetails) {
   const cardElement = document.createElement('div');
   cardElement.innerHTML = `
-		 <div class="game__card"
-		 data-game-card-id="${cardDetails.id}"
-		 data-game-card-status="closed"
-		 data-game-card-selected="${cardDetails.isSelected}"
-		 >
-			 <img
-				src="${cardDetails.src}"
-			  alt=""
-				class="game__card-image"
-		 />
-		  </div>
+  	<div class="game__card"
+	  data-game-card-id="${cardDetails.id}"
+	  data-game-card-status="closed"
+	  data-game-card-selected="${cardDetails.isSelected}"
+		>
+  		<div class="game__card-flipper">
+	 		<div class="game__card-front">
+				<span class="game__card-icon">?</span>
+	 		</div>
+	 	<div class="game__card-back">
+			<img src="${cardDetails.src}" alt="" class="game__card-image" />
+	 	</div>
+  		</div>
+	</div>
 	 `;
   return cardElement;
 }
