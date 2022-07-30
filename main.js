52cf88efc2471a989d8a94a7349cfa26ccc6465b
Vlad Kovalov <vlad.kovalov.92@gmail.com>
2022-07-30 19:01:08 +0300

    added card creation and adding func

--- main.js
+++ main.js
@@ -1,22 +1,48 @@
+const gameContent = document.querySelector('[data-game-content]');
+
 const cardImages = [
-  'img-1.jpg',
-  'img-2.jpg',
-  'img-3.jpg',
-  'img-4.jpg',
-  'img-5.jpg',
-  'img-6.jpg',
-  'img-7.jpg',
-  'img-8.jpg',
+  'img-1.png',
+  'img-2.png',
+  'img-3.png',
+  'img-4.png',
+  'img-5.png',
+  'img-6.png',
+  'img-7.png',
+  'img-8.png',
 ];
 
 const cardImagePath = './assets/images';
 
 const shuffledImages = [...cardImages, ...cardImages]
   .sort(() => 0.5 - Math.random())
-  .map((card, index) => ({
+  .map((imageName, index) => ({
     id: index,
     isSelected: false,
-    card,
+    src: `${cardImagePath}/${imageName}`,
   }));
 
-console.log(shuffledImages);
+function createGameCard(cardDetails) {
+  const cardElement = document.createElement('div');
+  cardElement.innerHTML = `
+		<div class="game__card"
+		data-game-card-id="${cardDetails.id}"
+		data-game-card-status="closed"
+		data-game-card-selected="${cardDetails.isSelected}"
+		>
+			<img
+	  		src="${cardDetails.src}"
+	 		alt=""
+	  		class="game__card-image"
+		/>
+ 		</div>
+	`;
+  return cardElement;
+}
+
+function addGameCard(container, cardElement) {
+  return container.appendChild(cardElement);
+}
+
+const cards = shuffledImages
+  .map((item) => createGameCard(item))
+  .forEach((card) => addGameCard(gameContent, card));
