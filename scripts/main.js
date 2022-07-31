8911015e95ea37c882461dd49ae7dcd6fe1cc3c2
Vlad Kovalov <vlad.kovalov.92@gmail.com>
2022-07-31 15:15:04 +0300

    fixed matching func

--- scripts/main.js
+++ scripts/main.js
@@ -21,45 +21,40 @@ gameContent.addEventListener('click', (event) => {
   if (isActive) {
     if (!event.target.closest('.game__card')) return;
 
-    const { gameCardName: cardName } =
-      event.target.closest('.game__card').dataset;
-
     if (!currentSelectedCard) {
-      currentSelectedCard = cardName;
-      event.target.closest(
-        '.game__card'
-      ).dataset.gameCardIsOpened = true;
+      currentSelectedCard = event.target.closest('.game__card');
+      currentSelectedCard.dataset.gameCardIsOpened = true;
       return;
     }
 
     if (currentSelectedCard) {
-      previousSelectedCard = currentSelectedCard;
-      currentSelectedCard = cardName;
-      event.target.closest(
-        '.game__card'
-      ).dataset.gameCardIsOpened = true;
+      previousSelectedCard = event.target.closest('.game__card');
+      previousSelectedCard.dataset.gameCardIsOpened = true;
+      console.log(
+        `Curr: ${currentSelectedCard.dataset.gameCardName}; Prev: ${previousSelectedCard.dataset.gameCardName}`
+      );
       isActive = false;
 
-      if (currentSelectedCard && previousSelectedCard) {
-        if (currentSelectedCard !== previousSelectedCard) {
-          setTimeout(() => {
-            document.querySelector(
-              `[data-game-card-name="${currentSelectedCard}"]`
-            ).dataset.gameCardIsOpened = false;
+      const { gameCardName: currName } = currentSelectedCard.dataset;
+      const { gameCardName: prevName } = previousSelectedCard.dataset;
 
-            document.querySelector(
-              `[data-game-card-name="${previousSelectedCard}"]`
-            ).dataset.gameCardIsOpened = false;
+      if (currName !== prevName) {
+        setTimeout(() => {
+          currentSelectedCard.dataset.gameCardIsOpened = false;
+          previousSelectedCard.dataset.gameCardIsOpened = false;
 
-            previousSelectedCard = '';
-            currentSelectedCard = '';
-            isActive = true;
-          }, 1500);
-        } else {
-          previousSelectedCard = '';
           currentSelectedCard = '';
+          previousSelectedCard = '';
           isActive = true;
-        }
+          return;
+        }, 1000);
+      }
+
+      if (currName === prevName) {
+        currentSelectedCard = '';
+        previousSelectedCard = '';
+        isActive = true;
+        return;
       }
     }
   }
